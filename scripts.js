01da5b761585c0950b328a0ea2efc879d4243a1a
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-17 19:23:29 -0400

    Create basic math function

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function add(firstOperand, secondOperand) {
+    return firstOperand + secondOperand;
+}
+
+function subtract(firstOperand, secondOperand) {
+    return firstOperand - secondOperand;
+}
+
+function multiply(firstOperand, secondOperand) {
+    return firstOperand * secondOperand;
+}
+
+function divide(firstOperand, secondOperand) {
+    return firstOperand / secondOperand;
+}
