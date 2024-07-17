296ec1deec807c4fe3d50e18bc038140e9deca0a
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-17 19:52:05 -0400

    Create HTML calculator elements

--- scripts.js
+++ scripts.js
@@ -10,3 +10,19 @@ let operations = {
 function operate(operator, firstOperand, secondOperand) {
     return (operations[operator])(firstOperand, secondOperand);
 }
+
+function createButtons() {
+    const buttonIDs = ["AC", "+/-", "%", "\u00F7", "7",
+                     "8", "9", "\u00D7", "4", "5", "6",
+                      "-", "1", "2", "3", "+", "0", ".", "="];
+    const calculatorContainer = document.querySelector(".calculator-container");
+
+    for (let id of buttonIDs) {
+        let button = document.createElement("button");
+        button.setAttribute("id", id);
+        button.textContent = id;
+        calculatorContainer.appendChild(button);
+    }
+}
+
+createButtons();
