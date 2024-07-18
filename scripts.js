eee6fdb98fd861e380e1182cbc3814805bb57d32
ahmeda03 <121465216+ahmeda03@users.noreply.github.com>
2024-07-17 22:47:29 -0400

    Apply styles to construct calculator

--- scripts.js
+++ scripts.js
@@ -15,14 +15,35 @@ function createButtons() {
     const buttonIDs = ["AC", "+/-", "%", "\u00F7", "7",
                      "8", "9", "\u00D7", "4", "5", "6",
                       "-", "1", "2", "3", "+", "0", ".", "="];
-    const calculatorContainer = document.querySelector(".calculator-container");
+    const buttonContainer = document.querySelector(".button-container");
 
     for (let id of buttonIDs) {
         let button = document.createElement("button");
-        button.setAttribute("id", id);
-        button.textContent = id;
-        calculatorContainer.appendChild(button);
+        applyButtonStyles(id, button);
+        buttonContainer.appendChild(button);
     }
+}
+
+function applyButtonStyles(id, button) {
+    button.setAttribute("id", `${id}-button`);
+    button.classList.add("calculator-button");
+    button.textContent = id;
+
+    let colorClass = getButtonColor(id);
+    button.classList.add(`${colorClass}`);
+}
+
+function getButtonColor(id) {
+    let colorClass;
+    if (+id >= 0 && +id <= 9 || id === ".") {
+        colorClass = "number-button";
+    } else if (id === "+" || id === "-" || id === "\u00D7" || id === "\u00F7" || id ===  "=") {
+        colorClass = "operation-button";
+    } else {
+        colorClass = "misc-button";
+    }
+
+    return colorClass;
 }
 
 createButtons();
